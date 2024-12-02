2fb0e9e94d010ed69c90486a5c744292dd7052fb
akmy <akamayu@proton.me>
2024-12-02 22:37:10 +0800

    Add 2024 02 ab python

--- 2024/python.py
+++ 2024/python.py
@@ -8,3 +8,24 @@ def d01b(lines: list[str]):
 
     a, b = [*map(Counter, zip(*map(str.split, lines)))]
     return sum(int(k) * v * b.get(k, 0) for k, v in a.items())
+
+
+def d02a(lines: list[str]):
+    from operator import sub
+
+    parse = lambda x: map(int, x.split())
+    diffs = lambda x: set(map(sub, g := [*x], g[1:]))
+    valid = lambda x: not (x - {1, 2, 3}) or not (x - {-1, -2, -3})
+    check = lambda x: valid(diffs(parse(x)))
+    return sum(map(check, lines))
+
+
+def d02b(lines: list[str]):
+    from operator import sub
+
+    parse = lambda x: [*map(int, x.split())]
+    grows = lambda x: [x] + [x[:i] + x[i + 1 :] for i in range(len(x))]
+    diffs = lambda x: set(map(sub, x, x[1:]))
+    valid = lambda x: not (x - {1, 2, 3}) or not (x - {-1, -2, -3})
+    check = lambda x: any(map(valid, map(diffs, grows(parse(x)))))
+    return sum(map(check, lines))
